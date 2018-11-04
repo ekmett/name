23eab08b578980da405e350817c9fe65dcb97f54
mniip <mniip@mniip.com>
2018-11-04 06:13:10 +0300

    Fix intersection as well

--- src/Nominal/Internal/Trie-Goal.hs
+++ src/Nominal/Internal/Trie-Goal.hs
@@ -477,13 +477,14 @@ intersectionWithKey f = go where
     Nothing -> Nil
     Just lv -> Tip rk (f (A rk) lv rv)
   go l@(Node lk ln lm la) r@(Node rk rn rm ra)
-    | unsafeShiftR okk (min ln rn) > 0xf = Nil -- disjoint
-    | otherwise = case compare ln rn of 
-      LT | rb .&. rm == 0 -> Nil
-      LT -> go l $ indexSmallArray ra $ popCount $ rm .&. (rb-1) -- lookup
-      GT | lb .&. lm == 0 -> Nil -- contained, but disjoint
-      GT -> go (indexSmallArray la $ popCount $ lm .&. (lb-1)) r -- lookup
-      EQ -> runST $ do -- same level, merge
+    | wd > 0xf = Nil -- disjoint
+    | ln > rn = if lm .&. bd /= 0
+        then go (indexSmallArray la ldm) r
+        else Nil
+    | ln < rn = if rm .&. bd /= 0
+        then go l (indexSmallArray ra rdm)
+        else Nil
+    | otherwise = runST $ do -- same level, merge
         let cm0 = lm .&. rm
         cma <- newSmallArray (popCount cm0) undefined
         let loop 0 !i !um = small lk ln um <$> if um == cm0
@@ -499,15 +500,17 @@ intersectionWithKey f = go where
         loop cm0 0 0
     where
       okk = xor lk rk
-      rb = bit (fromIntegral (unsafeShiftR okk rn))
-      lb = bit (fromIntegral (unsafeShiftR okk ln))
+      wd = unsafeShiftR okk (max ln rn)
+      bd = bit (fromIntegral wd)
+      ldm = popCount $ lm .&. (bd - 1)
+      rdm = popCount $ rm .&. (bd - 1)
   go l@(Full lk ln la) r@(Node rk rn rm ra)
-    | unsafeShiftR okk (min ln rn) > 0xf = Nil -- disjoint
-    | otherwise = case compare ln rn of 
-      LT | rb .&. rm == 0 -> Nil
-      LT -> go l $ indexSmallArray ra $ popCount $ rm .&. (rb-1)
-      GT -> go (indexSmallArray la ld) r
-      EQ -> runST $ do -- same level, merge
+    | wd > 0xf = Nil -- disjoint
+    | ln > rn = go (indexSmallArray la (fromIntegral wd)) r
+    | ln < rn = if rm .&. bd /= 0
+        then go l (indexSmallArray ra rdm)
+        else Nil
+    | otherwise = runST $ do -- same level, merge
         cma <- newSmallArray (length ra) undefined
         let loop 0 !i !um = small lk ln um <$> if um == rm
               then unsafeFreezeSmallArray cma -- all elements made it through
@@ -523,15 +526,16 @@ intersectionWithKey f = go where
         loop rm 0 0
     where
       okk = xor lk rk
-      rb = bit (fromIntegral (unsafeShiftR okk rn))
-      ld = fromIntegral (unsafeShiftR okk ln)
+      wd = unsafeShiftR okk (max ln rn)
+      bd = bit (fromIntegral wd)
+      rdm = popCount $ rm .&. (bd - 1)
   go l@(Node lk ln lm la) r@(Full rk rn ra)
-    | unsafeShiftR okk (min ln rn) > 0xf = Nil -- disjoint
-    | otherwise = case compare ln rn of 
-      LT -> go l (indexSmallArray ra rd)
-      GT | lb .&. lm == 0 -> Nil -- contained, but disjoint
-      GT -> go (indexSmallArray la $ popCount $ lm .&. (lb-1)) r -- lookup
-      EQ -> runST $ do -- same level, merge
+    | wd > 0xf = Nil -- disjoint
+    | ln > rn = if lm .&. bd /= 0
+        then go (indexSmallArray la ldm) r
+        else Nil
+    | ln < rn = go l (indexSmallArray ra (fromIntegral wd))
+    | otherwise = runST $ do -- same level, merge
         cma <- newSmallArray (length la) undefined
         let loop 0 !i !um = small lk ln um <$> if um == lm
               then unsafeFreezeSmallArray cma -- all elements made it through
@@ -547,14 +551,14 @@ intersectionWithKey f = go where
         loop lm 0 0
     where
       okk = xor lk rk
-      rd = fromIntegral (unsafeShiftR okk rn)
-      lb = bit (fromIntegral (unsafeShiftR okk ln))
+      wd = unsafeShiftR okk (max ln rn)
+      bd = bit (fromIntegral wd)
+      ldm = popCount $ lm .&. (bd - 1)
   go l@(Full lk ln la) r@(Full rk rn ra)
-    | unsafeShiftR okk (min ln rn) > 0xf = Nil -- disjoint
-    | otherwise = case compare ln rn of 
-      LT -> go l (indexSmallArray ra rd)
-      GT -> go (indexSmallArray la ld) r
-      EQ -> runST $ do
+    | wd > 0xf = Nil -- disjoint
+    | ln > rn = go (indexSmallArray la (fromIntegral wd)) r
+    | ln < rn = go l (indexSmallArray ra (fromIntegral wd))
+    | otherwise = runST $ do
         cma <- newSmallArray 16 undefined
         let loop 16 !i !um = small lk ln um <$> if um == 16
               then unsafeFreezeSmallArray cma
@@ -567,7 +571,9 @@ intersectionWithKey f = go where
                 Nil -> loop (d+1) i um
                 ux  -> writeSmallArray cma i ux >> loop (d+1) (i+1) (um .|. b)
         loop 0 0 0
-    where okk = xor lk rk; rd = fromIntegral (unsafeShiftR okk rn); ld = fromIntegral (unsafeShiftR okk ln)
+    where
+      okk = xor lk rk
+      wd = unsafeShiftR okk (max ln rn)
 {-# inlineable intersectionWith #-}
 
 filterMap :: (a -> Maybe b) -> Trie a -> Trie b
