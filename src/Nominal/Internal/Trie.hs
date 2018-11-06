46f57d872c2ad8dd32d3c8eae08dbea99b931324
Edward Kmett <ekmett@gmail.com>
2018-11-06 03:12:14 -0500

    license tags

--- src/Nominal/Internal/Trie.hs
+++ src/Nominal/Internal/Trie.hs
@@ -22,7 +22,16 @@
 {-# OPTIONS_GHC -fno-full-laziness #-}
 #endif
 
--- natural maps
+---------------------------------------------------------------------------------
+-- |
+-- Copyright :  (c) Edward Kmett 2018
+-- License   :  BSD-2-Clause OR Apache-2.0
+-- Maintainer:  Edward Kmett <ekmett@gmail.com>
+-- Stability :  experimental
+-- Portability: non-portable
+--
+---------------------------------------------------------------------------------
+
 module Nominal.Internal.Trie
 {-
   ( Trie
--- src/Nominal/Support.hs
+++ src/Nominal/Support.hs
@@ -3,6 +3,17 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE LambdaCase #-}
+
+---------------------------------------------------------------------------------
+-- |
+-- Copyright :  (c) Edward Kmett 2018
+-- License   :  BSD-2-Clause OR Apache-2.0
+-- Maintainer:  Edward Kmett <ekmett@gmail.com>
+-- Stability :  experimental
+-- Portability: non-portable
+--
+---------------------------------------------------------------------------------
+
 module Nominal.Support where
 
 import Control.Monad.State
